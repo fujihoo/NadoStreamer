6ea79220f343232a3b3f94ed89235a3452d82d05
fujihoo@naver.com <fujihoo@naver.com>
2022-05-12 22:30:46 +0900

    first commit

---
+++ NadoStreamer/1.main.cpp
@@ -0,0 +1,21 @@
+﻿#include "GAME.h"
+#include <locale.h>
+
+int Opening();
+int GAME_Loop();
+int ONAIR_Loop(int*, int*, int*, int*, int*, int*, int*, int*);
+
+int main(void)
+{
+	setlocale(LC_ALL, "");
+	int i_dona = 0;
+	int i_now_dona = 0;
+	int i_all_dona = 0;
+	int i_inc_view = 0;
+	int i_dec_view = 0;
+	int i_now_view = 0;
+	int i_all_view = 0;
+	int i_day = 1;
+
+	ONAIR_Loop(&i_dona, &i_now_dona, &i_all_dona, &i_inc_view, &i_dec_view, &i_now_view, &i_all_view, &i_day);
+}
---
+++ NadoStreamer/GAME.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "ONAIR.h"
+
